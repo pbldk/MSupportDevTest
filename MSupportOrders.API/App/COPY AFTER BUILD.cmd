4e9b00a1ac16448de06e44ec025c3a0c9083be61
Elitenet Denmark <customerservice@elitenet.dk>
2020-04-17 16:04:42 +0200

    remove inline styling

--- MSupportOrders.API/App/COPY AFTER BUILD.cmd
+++ MSupportOrders.API/App/COPY AFTER BUILD.cmd
@@ -1,7 +1,7 @@
-set loc=W:\REPO\MSupportOrders\MSupportOrders.API\App\
+set loc=W:\REPO\MSupportDevTest\MSupportOrders.API\App\
 chdir /d %loc%
 CD templates
-COPY *.html W:\REPO\MSupportOrders\MSupportOrders.API\_publish\scripts\templates\
+COPY *.html W:\REPO\MSupportDevTest\MSupportOrders.API\_publish\scripts\templates\
 CD ..
 CD views
-COPY *.html W:\REPO\MSupportOrders\MSupportOrders.API\_publish\scripts\views\
+COPY *.html W:\REPO\MSupportDevTest\MSupportOrders.API\_publish\scripts\views\
